3df814ad69abd594932e3f5873f07c69a7c469e1
hmallen <allenhm@gmail.com>
2020-12-20 20:12:35 -0700

    Added API testing and datetime conversion.

--- dn_test.py
+++ dn_test.py
@@ -1,22 +1,55 @@
 from pprint import pprint
+import os
+import time
 
+from pymongo import MongoClient
 from vanderwaals import coinwrangler
 from vanderwaals.utils import datanormalizer
+
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
 
 
 if __name__ == '__main__':
     cw = coinwrangler.CoinWrangler()
     dn = datanormalizer.DataNormalizer()
 
-    gecko_coins = cw.get_gecko_coins()
-    #paprika_coins = cw.get_paprika_coins()
+    mongo_coll = MongoClient(os.getenv('MONGO_ATLAS'))['vanderwaals']['apitesting']
+    
+    recording_active = True
+    while recording_active is True:
+        try:
+            gecko_coins = cw.get_gecko_coins()
+            paprika_coins = cw.get_paprika_coins()
 
-    for coin in gecko_coins:
-        pprint(dn.unify_gecko(coin))
+            for idx, coin in enumerate(gecko_coins):
+                #pprint(dn.unify_gecko(coin))
+                gecko_coins[idx] = dn.unify_gecko(coin)
+            
+            insert_result = mongo_coll.insert_many(gecko_coins)
+            print(f'Gecko Coins Inserted: {len(insert_result.inserted_ids)}')
 
-    #for coin in paprika_coins:
-    #    pprint(dn.unify_paprika(coin))
+            for idx, coin in enumerate(paprika_coins):
+                #pprint(dn.unify_paprika(coin))
+                paprika_coins[idx] = dn.unify_paprika(coin)
+            
+            insert_result = mongo_coll.insert_many(paprika_coins)
+            print(f'Paprika Coins Inserted: {len(insert_result.inserted_ids)}')
 
-    #pprint(gc_norm)
-    #print()
-    #pprint(pc_norm)
+            delay_start = time.time()
+            update_last = 0
+            while (time.time() - delay_start) < 300:
+                if (time.time() - update_last) >= 30:
+                    print(f'{300 - (time.time() - delay_start)} seconds remaining until coin data update.')
+                    update_last = time.time()
+                time.sleep(1)
+    
+        except KeyboardInterrupt:
+            print('Exit signal received.')
+            recording_active = False
+        
+        except Exception as e:
+            print(e)
+            time.sleep(10)
+    
+    print('Done.')
